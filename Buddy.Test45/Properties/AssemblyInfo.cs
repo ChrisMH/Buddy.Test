ad2f58adeee16e5de6acdc8c7c32a2ff87d4c1a3
Chris Hogan <hogancm@gmail.com>
2017-02-18 16:21:46 -0500

    Changes to get embedded resources to work for all versions

--- Buddy.Test45/Properties/AssemblyInfo.cs
+++ Buddy.Test45/Properties/AssemblyInfo.cs
@@ -4,5 +4,3 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Buddy.Test")]
 [assembly: AssemblyDescription("Buddy.Test Classes")]
 [assembly: AssemblyConfiguration("")]
-
-[assembly: InternalsVisibleTo("Buddy.Test.Test")]
